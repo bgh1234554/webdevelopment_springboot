a84c009a446b2e8fa2339eabee072895843cb11c
unknown <bgh1234554@gmail.com>
2024-12-24 19:21:37 +0900

    Chapter 01 생성

---
+++ chapter01/src/main/java/me/BaekJiHoon/BaekJiHoon/Main.java
@@ -0,0 +1,12 @@
+package me.BaekJiHoon.BaekJiHoon;
+/*
+    스프링부트 3 프로젝트 만들기
+    1단계 - 현재까지 내가 만든 프로젝트는 Gradle 프로젝트를 생성한 것이다.
+    그레이들 프로젝트를 스프링부트 3 프로젝트로 만들예정.
+    build.gradle -> gradle 설정 파일
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
